05932f04f13448b1d10d420850073bca482e151f
Methonach <chamroeunchanmithona755@gmail.com>
2023-07-26 14:40:06 +0700

    add

---
+++ controllers/userControllers.js
@@ -0,0 +1,21 @@
+const con = require('../config/db');
+const getAll = (req, res) => {
+    con.query('SELECT * FROM `user`', (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+        res.render('user/index', {result});
+        }
+    });
+    
+}
+
+
+
+
+
+
+
+module.exports = {
+    getAll,
+}
